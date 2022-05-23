78f3bbdfa213c9e951ac23e888ab20442e60563d
Mikael Imboden <imbodenm@bzz.ch>
2022-05-23 23:34:05 +0200

    working service vermietungen/skipisten

--- src/main/java/ch/bzz/skigebiete/service/TestService.java
+++ src/main/java/ch/bzz/skigebiete/service/TestService.java
@@ -1,4 +1,4 @@
-package service;
+package ch.bzz.skigebiete.service;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -7,7 +7,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
- * test service
+ * test ch.bzz.skigebiete.service
  */
 @Path("test")
 public class TestService {
